e353e51871933e7ebd6a71670795666f5e7f8752
Taha Topal <taha.topal.7@gmail.com>
2022-03-04 14:44:17 +0300

    discovery service & api gateway

--- users-ws/src/main/java/com/project/UsersWsApplication.java
+++ users-ws/src/main/java/com/project/UsersWsApplication.java
@@ -3,11 +3,13 @@ package com.project;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsersWsApplication {
 
     public static void main(String[] args) {
