7108073090292e0fe3c4b0df88df18ccf0c0f1f4
Taha Topal <taha.topal.7@gmail.com>
2022-03-20 20:35:50 +0300

    last commit

--- users-ws/src/main/java/com/project/controller/AuthenticationController.java
+++ users-ws/src/main/java/com/project/controller/AuthenticationController.java
@@ -14,9 +14,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 @RestController
 public class AuthenticationController {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -31,9 +34,11 @@ public class AuthenticationController {
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
 
         try {
+            logger.info("Preparing to authenticate...");
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
             );
+            logger.info("Authtenciation is successful!");
         }
         catch (BadCredentialsException e) {
             throw new Exception("Incorrect username or password", e);
--- users-ws/src/main/java/com/project/security/WebSecurity.java
+++ users-ws/src/main/java/com/project/security/WebSecurity.java
@@ -32,8 +32,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
-                .anyRequest().authenticated().and()
-                .exceptionHandling().and().sessionManagement()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
